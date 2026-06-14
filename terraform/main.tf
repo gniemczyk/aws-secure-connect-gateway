06c7856ffe0a3fe7a9df2652d9f8d1a201838b79
Grzegorz <26359006+gniemczyk@users.noreply.github.com>
2026-06-14 12:58:46 +0200

    fix: gh workflow, terraform, trivyignore

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,7 @@ resource "aws_ecr_repository" "bastion" {
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
-    scan_on_push = false
+    scan_on_push = true
   }
 
   # Pozwala usunąć repozytorium nawet z obrazami przy terraform destroy
